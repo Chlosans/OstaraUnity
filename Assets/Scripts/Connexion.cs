2d712a645bd3ca23d58fbbf48b0104c824e3b267
Chloé Sans <sansofchloe@gmail.com>
2020-04-23 01:32:48 +0200

    Design
    Page Connexion Design
    Cinématique ajout fond

--- Assets/Scripts/Connexion.cs
+++ Assets/Scripts/Connexion.cs
@@ -2,21 +2,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+
 using UnityEngine;
 using UnityEngine.UI;
 
 
 public class Connexion : MonoBehaviour
 {
-    public GameObject codeText;
-    private int codeRamdon;
+    public Text codeText;
+    private int codeRamdon1;
+    private int codeRamdon2;
+    private int codeRamdon3;
+    private int codeRamdon4;
+    private int codeRamdonglobal;
+    
 
     private void Start()
     {
 
-        codeRamdon = UnityEngine.Random.Range(1000,9999);
-        Debug.Log(codeRamdon);
-        codeText.GetComponent<TextMeshProUGUI>().text =  ""+codeRamdon ;
+        codeRamdon1 = UnityEngine.Random.Range(1,9);
+        codeRamdon2 = UnityEngine.Random.Range(1,9);
+        codeRamdon3 = UnityEngine.Random.Range(1,9);
+        codeRamdon4 = UnityEngine.Random.Range(1,9);
+        codeText.text = "" + codeRamdon1 + " " + codeRamdon2 + " " + codeRamdon3 + " " + codeRamdon4;
+
+        codeRamdonglobal = int.Parse(codeRamdon1.ToString() + codeRamdon2.ToString() + codeRamdon3.ToString() + codeRamdon4.ToString());
+    
     }
     
     
